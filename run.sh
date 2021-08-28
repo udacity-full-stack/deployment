6ca07f19f2cf6255a990edd87968be2a21a43e9b
Alberto <albertjmrs@gmail.com>
2021-08-29 00:20:57 +0100

    add deployment changes

--- run.sh
+++ run.sh
@@ -57,6 +57,61 @@ case $1 in
     echo ${NEW_CONTAINER_ID} > .run_status
     ;;
 
+  # ********************************************* 
+  # **** DEPLOYMENT COMMANDS (ORDER MATTERS) ****
+
+  create-cluster)
+    eksctl create cluster --name simple-jwt-api
+    ;;
+
+  create-role)
+    aws iam create-role --role-name UdacityFlaskDeployCBKubectlRole --assume-role-policy-document file://deployment/trust.json --output text --query 'Role.Arn'
+    aws iam put-role-policy --role-name UdacityFlaskDeployCBKubectlRole --policy-name eks-describe --policy-document file://deployment/iam-role-policy.json
+    ;;
+
+  get-cluster-config)
+    kubectl get -n kube-system configmap/aws-auth -o yaml > ./deployment/aws-auth-patch.yml
+    # the above will save the current config to deployment/aws-auth-patch.yml
+    # modify it and add under data
+        # mapRoles: |
+        #     - groups:
+        #         - system:masters
+        #         rolearn: arn:aws:iam::<ACCOUNT_ID>:role/UdacityFlaskDeployCBKubectlRole
+        #         username: build  
+    ;;
+
+  allow-role-to-access-cluster)
+    kubectl patch configmap/aws-auth -n kube-system --patch "$(cat ./deployment/aws-auth-patch.yml)"
+    ;;
+
+  # NEXT: create a stack by using the template ci-cd-codepipeline.cfn.yml in the AWS CONSOLE
+
+  set-secret) 
+    #   usage: ./run.sh set-secret thisISaSECRET
+    aws ssm put-parameter --name JWT_SECRET --overwrite --value "$2" --type SecureString
+    ;;
+
+  # *************************** 
+  # **** CLEAN UP COMMANDS ****
+
+  remove-cluster)
+    eksctl delete cluster simple-jwt-api
+    ;;
+
+  remove-stack)
+    aws cloudformation delete-stack --stack-name simple-jwt-api-pipeline
+    ;;
+
+  remove-secret)
+    aws ssm delete-parameter --name JWT_SECRET
+    ;;
+
+  remove)
+    ./run.sh remove-cluster
+    ./run.sh remove-stack
+    ./run.sh remove-secret
+    ;;
+
   *)
     echo "Command $1 not implemented, exiting..."
 esac
